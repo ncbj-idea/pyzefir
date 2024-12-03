f5cf2d954dfb6ae32894fa4a2379269364172720
Michał Marszal <michal.marszal@idea.edu.pl>
2024-12-03 14:56:35 +0100

    Update xarray dependency

--- pyzefir/__init__.py
+++ pyzefir/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.4.31"
+__version__ = "0.4.32"
 
 import os
 from pathlib import Path
